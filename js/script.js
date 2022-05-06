6100a1852aa08a2a3ff161a20ab2a3429601a601
KARTHIKA <karthikamanikantanm7@gmail.com>
2022-05-06 16:13:15 +0530

    axit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function one(){
+    
+    document.getElementById("text-1").style.display="block";
+    document.getElementById("text-2").style.display="none";
+    document.getElementById("text-3").style.display="none";
+
+}
+function two(){
+    document.getElementById("text-2").style.display="block";
+    document.getElementById("text-1").style.display="none";
+    document.getElementById("text-3").style.display="none";
+}
+function three(){
+    document.getElementById("text-3").style.display="block";
+    document.getElementById("text-1").style.display="none";
+    document.getElementById("text-2").style.display="none";
+}
